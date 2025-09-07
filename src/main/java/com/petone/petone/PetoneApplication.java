a6a858572d53cd20b4e1f4e4d44a3158c2f8a0c4
Levi Junior <139442686+juniorlevi11@users.noreply.github.com>
2025-09-07 17:56:51 +0000

    feat: security opa in dev + endpoint de sáude

---
+++ src/main/java/com/petone/petone/PetoneApplication.java
@@ -0,0 +1,11 @@
+package com.petone.petone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetoneApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PetoneApplication.class, args);
+  }
+}
