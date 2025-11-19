c90ec7fad999d64189e0777586efceae7d297a13
HenriqueAStoppa <111657733+HenriqueAStoppa@users.noreply.github.com>
2025-11-19 15:01:21 -0300

    front novo e teste da classe principal

--- src/main/java/com/petone/petone/PetoneApplication.java
+++ src/main/java/com/petone/petone/PetoneApplication.java
@@ -2,11 +2,17 @@ package com.petone.petone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
  * Ponto de entrada principal da aplicação Spring Boot.
  */
+
 @SpringBootApplication
+
+@EnableMongoRepositories(basePackages = "com.petone.petone.repository")
+@ComponentScan(basePackages = "com.petone.petone")
 public class PetoneApplication {
 
     public static void main(String[] args) {
